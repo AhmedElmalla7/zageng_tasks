1f7069d6ffbc2d223bd6fcb27e336bccae310c0e
Ahmed Elmallah <elmallaha59@gmail.com>
2026-04-14 22:22:00 +0200

    Add files via upload

---
+++ Task_Feedback_API/Controllers/FeedbackController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_Feedback_API.Models;
+
+namespace Task_Feedback_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger _logger;
+        public FeedbackController(IConfiguration config , ILogger<FeedbackController> logger)
+        {
+            _config = config; 
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult PostFeedback( /*[FromQuery] int rate ,*/ FeedbackRequest request)
+        {
+            var systemName = _config["SystemSettings:SystemName"];
+
+            _logger.LogInformation("Feedback received from user : {UserName}", request.UserName);
+
+            if(request.Rate < 3)
+            {
+                _logger.LogWarning("User {UserName} is not satisfied with the service",request.UserName);
+            }
+
+            var message = $"Feedback received for {systemName} . Thank you, {request.UserName}";
+
+            return Ok(message);
+        }
+
+    }
+}
---
+++ Task_Feedback_API/Models/FeedbackRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Task_Feedback_API.Models
+{
+    public class FeedbackRequest
+    {
+        public string UserName { get; set; }
+
+        [Range(1 , 5 , ErrorMessage = "Rate must be [1 : 5]")]
+        public int Rate { get; set; }
+        public string Comment { get; set; }
+    }
+}
